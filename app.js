0c425341fb4ab873ca0a5cde2c4a7a2e2cd11d76
njanne19 <nickjanne7@gmail.com>
2017-04-28 23:35:56 -0400

    Procfile

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var logger = require('morgan');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+
 
 var app = express();
 
@@ -16,7 +16,6 @@ app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,33 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.render('index');
+});
+
+router.get('/:dateVal', function(req, res) {
+  var dateVal = req.params.dateVal;
+//Natural Date Options
+  var dateFormatOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  };
+
+
+
+  if (isNaN(dateVal)) {
+    var naturalDate = new Date(dateVal);
+    naturalDate = naturalDate.toLocaleDateString('en-us', dateFormatOptions);
+
+    var unixDate = new Date (dateVal).getTime()/1000;
+  } else {
+    var unixDate = dateVal;
+    var naturalDate = new Date(dateVal * 1000);
+    naturalDate = naturalDate.toLocaleDateString('en-us', dateFormatOptions);
+  }
+  res.json({unix: unixDate, natural: naturalDate});
+
 });
 
 module.exports = router;
